7316573df5a0711c32b3830965063041ace21935
cplan85 <carlos_planchart@live.com>
2022-06-08 19:55:13 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { Employee } from './employee.model';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app-employees';
+  title = 'List of Employees';
+
+  employees: Employee[] = [
+    new Employee('John', 'Wick', 'assassin', 7500),
+    new Employee('John', 'Lopez', 'diver', 8500),
+    new Employee('Laura', 'Mendez', 'swimmer', 10500),
+    new Employee('John', 'Wick', 'assassin', 7500),
+  ];
+
+  addEmployee(): void {
+    this.employees.push(new Employee('John', 'Wick', 'assassin', 7500));
+  }
 }
