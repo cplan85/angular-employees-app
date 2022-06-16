65990d0975ad69a507b630e02c01e8f1195acf01
cplan85 <carlos_planchart@live.com>
2022-06-16 20:37:22 +0200

    implemented renderer2 in view-child

--- src/app/view-child/view-child.component.ts
+++ src/app/view-child/view-child.component.ts
@@ -1,17 +1,43 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-view-child',
   templateUrl: './view-child.component.html',
-  styleUrls: ['./view-child.component.css']
+  styleUrls: ['./view-child.component.css'],
 })
 export class ViewChildComponent implements OnInit {
   @ViewChild('asTitle') title: ElementRef;
   @ViewChild('asImage') image: ElementRef;
-  //constructor(private renderer2: Renderer2) { }
+
+  public data: any;
+  constructor(private renderer2: Renderer2) {}
 
   ngOnInit(): void {
-
+    this.data = {
+      text: 'Hello world?',
+      url: 'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg',
+    };
   }
 
+  change(): void {
+    const asTitle = this.title.nativeElement;
+    const asImage = this.image.nativeElement;
+
+    this.renderer2.setStyle(asTitle, 'color', 'red');
+
+    this.renderer2.setAttribute(
+      asImage,
+      'src',
+      'https://www.wpbeginner.com/wp-content/uploads/2020/03/ultimate-small-business-resource-coronavirus.png'
+    );
+
+    this.renderer2.setAttribute(asImage, 'data-name', 'pepito');
+    console.log(asTitle);
+  }
 }
